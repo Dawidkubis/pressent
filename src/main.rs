192c6f4526a01206e10da10ae4d415226572f908
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-01-27 14:59:20 +0100

    yeehaw

--- src/main.rs
+++ src/main.rs
@@ -11,53 +11,25 @@ extern crate serde;
 extern crate structopt;
 extern crate toml;
 
-#[macro_use]
-mod response;
-#[macro_use]
-mod settings;
 mod cli;
 mod routes;
-mod rsp;
 
 use cli::Cli;
-use settings::Settings;
 use std::{env, thread, time};
 use structopt::StructOpt;
-
-pub const SETTINGS_PATH: &str = "settings.toml";
-pub const WWW: &str = "www";
-
-lazy_static! {
-	pub static ref SETTINGS: Settings = match Settings::get() {
-		Ok(s) => s,
-		Err(e) => panic!("Unable to parse {} : {}", SETTINGS_PATH, e),
-	};
-}
 
 fn main() {
 	// get cmd args
 	let opt = Cli::from_args();
-
-	// check settings
-	&*SETTINGS;
 
 	// port setting
 	if let Some(i) = opt.port {
 		env::set_var("ROCKET_PORT", format!("{}", i));
 	}
 
-	// git repo update
-	thread::spawn(|| loop {
-		thread::sleep(time::Duration::from_secs(60));
-		match SETTINGS.git.update() {
-			Ok(s) => println!("git repo update: status = {}", s.status),
-			Err(e) => eprintln!("{:?}", e),
-		}
-	});
-
 	// rocket server init
 	rocket::ignite()
-		.mount("/", routes![routes::index, routes::path,])
+		.mount("/", routes![routes::index])
 		.register(catchers![routes::not_found,])
 		.launch();
 }
