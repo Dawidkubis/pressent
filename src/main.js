28ebf579a51b0b99e50ea2ea9ade3b7066236cdb
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-02-03 15:50:10 +0100

    upgrade

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('keydown', evt => {
-    if (evt.key === 'ArrowLeft') {
+    if (evt.key === 'ArrowLeft' || evt.key === "Prev" || evt.key === "PageUp") {
 		window.location.pathname = Number(window.location.pathname.slice(1)) - 1;
-    } else if (evt.key === 'ArrowRight') {
+    } else if (evt.key === 'ArrowRight' || evt.key === "Next" || evt.key === "PageDown") {
 		window.location.pathname = Number(window.location.pathname.slice(1)) + 1;
     }
 }, false);
