03f37e95eda83d3d2639b434ce5290cf52e0d179
Dawid J. Kubis <dawidkubis@hitler.rocks>
2020-10-15 18:27:01 +0200

    .

--- src/response.rs
+++ src/response.rs
@@ -15,13 +15,20 @@ impl MarkDown {
 		let css: String = MAIN.to_owned();
 		let js: String = JS.to_owned();
 
-		let markdown = markdown_to_html(s, &ComrakOptions::default());
+		let options = ComrakOptions {
+			unsafe_: true,
+			..ComrakOptions::default()
+		};
+
+		let markdown = markdown_to_html(s, &options);
 
 		format!(
 			"
 <!DOCTYPE html>
 <html>
 	<head>
+		<script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>
+		<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script>
 		<style>{}</style>
 		<script>{}</script>
 		<title>{}</title>
